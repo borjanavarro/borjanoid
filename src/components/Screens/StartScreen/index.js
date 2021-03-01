90e758af94069a376a6072f6812ba56f6e0db657
Borja <borja.navarro.web@gmail.com>
2021-03-01 12:59:03 +0100

    Desktop restriction added

--- src/components/Screens/StartScreen/index.js
+++ src/components/Screens/StartScreen/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { WAIT_SCREEN } from '../constants';
 
 import './styles.scss'
 
 function StartScreen({ setScreen }) {
+    const [message, setMessage] = useState('');
+    const boardData = useSelector(state => state.board);
+    let height = boardData.topMaxPos - boardData.topMinPos;
+    let width = boardData.leftMaxPos - boardData.leftMinPos;
+
+    const startButton = () => {
+        if ( window.innerHeight > height && window.innerWidth > width + 2 * 280 ) {
+            setScreen(WAIT_SCREEN)
+        } else {
+            setMessage("Your device hasn't enough resolution to play this game");
+        }
+    }
 
     return (
         <div className="start-container">
@@ -11,9 +24,8 @@ function StartScreen({ setScreen }) {
                 <h1>Arkanoid</h1>
                 <h3>The classic Arkanoid game</h3>
                 <p className="instructions">use arrow keys to play</p>
-                <div className="button-container">
-                    <button onClick={ (e) => setScreen(WAIT_SCREEN) }>Start game</button>
-                </div>
+                <button onClick={ startButton }>Start game</button>
+                <p className="error">{ message }</p>
                 <div className="bottom">
                     <h5>code & design by <a href="https://www.linkedin.com/in/borja-navarro-web/" target="_blank" rel="noreferrer">Borja Navarro</a></h5>
                 </div>
